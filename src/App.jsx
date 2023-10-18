21ca661fc37a3f189cf334e9157648f55e3337b3
Harsha Yuvaraj <harsha55raj@gmail.com>
2023-10-18 10:37:42 -0400

    Boiler Plate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+const API_KEY = import.meta.env.APP_API_KEY;
+
+function App() {
+
+  return (
+    <>
+       <p>Boiler Plate</p>
+    </>
+  )
+}
+
+export default App
